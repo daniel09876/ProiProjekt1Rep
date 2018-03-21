47747f7e1bcc8e7be9dca86377fa5d3ea1417710
daniel09876 <daniel.chylewski@gmail.com>
2018-03-22 00:48:51 +0100

    Branch1

--- ProiProjekt1/main.cpp
+++ ProiProjekt1/main.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+string ChangingVowels(string str) {
+  int strlen = str.length();
+  for (int i=0; i<strlen;i++) {
+    if(str[i] == 'a'||str[i] == 'e'||str[i] == 'y'||str[i] == 'o'||
+        str[i] == 'u'||str[i] == 'i'||str[i] == 'A'||str[i] == 'E'||
+        str[i] == 'Y'||str[i] == 'O'||str[i] == 'U'||str[i] == 'I') str[i] = 'z';
+  }
+  return str;
+}
+
 int main ()
 {
   string name,surname,pseudonym;
@@ -11,7 +21,8 @@ int main ()
   cin>>surname;
   cout<<" Write pseudonym:"<<endl;
   cin>>pseudonym;
-  cout<<" Output:\n"<<name<<" \""<<pseudonym<<"\" "<<surname<<endl;
+  cout<<" Output:\n"<<ChangingVowels(name)<<" \""<<ChangingVowels(pseudonym)<<"\" "
+    <<ChangingVowels(surname)<<endl;
   
   return 0;
 } 
