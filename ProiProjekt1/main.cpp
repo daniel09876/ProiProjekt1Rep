0b3a009798890b007c1d365bf808aa1b1ff4d658
daniel09876 <daniel.chylewski@gmail.com>
2018-03-21 23:44:18 +0100

    Master1

---
+++ ProiProjekt1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+  string name,surname,pseudonym;
+  cout<<" Write name:"<<endl;
+  cin>>name;
+  cout<<" Write surname:"<<endl;
+  cin>>surname;
+  cout<<" Write pseudonym:"<<endl;
+  cin>>pseudonym;
+  cout<<" Output:\n"<<name<<" \""<<pseudonym<<"\" "<<surname<<endl;
+  
+  return 0;
+} 
