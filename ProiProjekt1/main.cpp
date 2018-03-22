0238824cecdad52ca6578c1e7bac7f9b828f3027
daniel09876 <daniel.chylewski@gmail.com>
2018-03-22 01:25:21 +0100

    Branch2 Comm1

--- ProiProjekt1/main.cpp
+++ ProiProjekt1/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,10 +8,13 @@ int main ()
   string name,surname,pseudonym;
   cout<<" Write name:"<<endl;
   cin>>name;
+  reverse(name.begin(),name.end());
   cout<<" Write surname:"<<endl;
   cin>>surname;
+  reverse(surname.begin(),surname.end());
   cout<<" Write pseudonym:"<<endl;
   cin>>pseudonym;
+  reverse(pseudonym.begin(),pseudonym.end());
   cout<<" Output:\n"<<name<<" "<<surname<<" \""<<pseudonym<<"\""<<endl;
   
   return 0;
